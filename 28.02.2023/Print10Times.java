5011af8a56364eeb19ae26c0e81f8283c719ba3a
Ishank Khandelwal <ishank098ik@gmail.com>
2023-03-01 01:37:53 +0530

    first

---
+++ 28.02.2023/Print10Times.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Print10Times {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "Ishank ";
+        char ch = s.charAt(0);
+        System.out.println(ch);
+        System.out.println(s.charAt(1));
+        System.out.println(s.charAt(2));
+        System.out.println(s.charAt(3));
+        System.out.println(s.charAt(4));
+        System.out.println(s.charAt(6));
+       
+
+
+
+    }
+    
+}
---
+++ 28.02.2023/Variables.java
@@ -0,0 +1,39 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Basic Variables
+        int i = 330000;
+        System.out.println(i);
+
+
+        short s = 32000;
+        System.out.println(s);
+
+        long l = 3000000;
+        System.out.println(l);
+
+        float f2 = 3.142356789f;
+        System.out.println(f2);
+
+        double d = 1.20414681697789742897897;
+        System.out.println(d);
+
+        char c1 = 'A', c2 = 'a', c3 = '@', c4 = '1';
+        System.out.println(c1 +" "+ c2 +" "+ c3 +" "+ c4);  // +" "+  String concatination
+
+        boolean b1 = true , b2 = false;
+        System.out.println(b1 +" abcd " );
+
+
+        float f = (float)3.142356789;  //double to float conversion
+        System.out.println(f);
+
+        int x = (int)3.1432;  // double to int conversion
+        System.out.println(x);
+
+        
+
+
+
+    }
+    
+}
