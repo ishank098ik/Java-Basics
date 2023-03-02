d8292fbffb507d58d79935a45617f61e13911096
Ishank Khandelwal <ishank098ik@gmail.com>
2023-03-02 22:04:29 +0530

    march02 2023

---
+++ March02_2023/Patterns.java
@@ -0,0 +1,115 @@
+package March02_2023;
+
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+
+        // SOLID RECT
+        
+    // int rows = 4;
+    // int columns = 5;
+
+        // outer loop
+
+    // for(int i = 1; i <= rows; i++){
+
+        // inner loop
+
+    //     for(int j = 1; j <= columns; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //     }
+
+                // HOLLOW RECT
+
+    // int n = 4;
+    // int m = 5;
+
+    // for(int i = 1; i <= n; i++){
+    //     for(int j = 1; j <= m; j++){
+    //         if(i == 1 || j == 1 || i == n || j == m){
+    //             System.out.print("*");
+    //         } else{
+    //             System.out.print(" ");
+    //         }
+    //     }
+    //     System.out.println();
+    //     }
+
+                // HALF PYRAMID       
+
+    // int n = 4;
+    // for(int i = 1; i <= n; i++){
+    //     for(int j = 1; j <= i; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+            // inverted
+
+        // int n = 4;
+        // for(int i = n; i >= 1; i--){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+                // HALF PYRAMID NUMBERS
+        
+        // int n = 5;
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+                // INVERTED
+
+        // int n = 5;
+
+        // for(int i = 1; i >= n; i++){
+        //     for(int j = n-i+1; j <= n+i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+                // FLOYD TRIANGLE 
+
+        // int n = 5;
+        // int number = 1;
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print(number + " ");
+        //         number++;  // number increse 
+        //     }
+        //     System.out.println();
+        // }
+
+                // 0 1 TRIANGLE
+
+            //    int n = 5;
+               
+            //    for(int i=1; i<=n;i++){
+            //     for(int j=1; j<=i; j++){
+            //         int sum= i+j;
+            //         if(sum % 2 = 0){
+            //             System.out.print("1 ");
+
+            //         } else {
+            //             System.out.print("0 ");
+            //         }
+            //     }
+            //     System.out.println();
+            //    }
+            
+    }
+    
+}
