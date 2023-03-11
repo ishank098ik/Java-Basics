65356a6a93cc3f9ce0843d599b65ecb49da01e5c
Ishank Khandelwal <ishank098ik@gmail.com>
2023-03-11 21:27:12 +0530

    march11 2023

--- March11_2023/Duplicate_Missing.java
+++ March11_2023/Duplicate_Missing.java
@@ -2,7 +2,7 @@ package March11_2023;
 
 import java.util.*;
 
-public class MissingNumber {
+public class Duplicate_Missing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
@@ -11,18 +11,14 @@ public class MissingNumber {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] != i+1){ // i != arr[i]-1
-                System.out.print(i+1); // System.out.print(arr[i]-1);
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i] == arr[i+1]){
+                System.out.println(arr[i]);
+                System.out.print(arr[i]+1);
                 break;
             }
-        }
-        if(arr[size-1] == size){
-            System.out.println(size+1);
         }
         
     }
     
 }
-// sout(arr[i])
-// sout(arr[i]+1)
